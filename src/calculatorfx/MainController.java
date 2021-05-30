e24c421c3bc30348d04ba78fc1ec23cf1fcaae66
Solomon King <50495497+1solomonking@users.noreply.github.com>
2021-05-30 13:20:09 +0100

    Add files via upload

---
+++ src/calculatorfx/MainController.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculatorfx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author CySecurity Increment
+ */
+public class MainController {
+    @FXML
+    private Label result;
+    private long number1 = 0;
+    private String operator = "";
+    private boolean start = true;
+    private Model model = new Model();
+    
+    @FXML
+    public void processNumbers(ActionEvent event){
+        if (start) {
+            result.setText("");
+            start = false;
+        }
+        String value = ((Button)event.getSource()).getText();
+        result.setText(result.getText()+ value);
+    }
+    
+    @FXML
+    public void proccessOperators(ActionEvent event){
+        String value = ((Button)event.getSource()).getText();
+        if (!value.equals("=")){
+            if (!operator.isEmpty()){
+                return;
+            }
+            operator = value;
+            number1 = Long.parseLong(result.getText());
+            result.setText("");
+        }else{
+            if (operator.isEmpty())
+                return;
+            long number2 = Long.parseLong(result.getText());
+            float output = model.calculate(number1, number2, operator);
+            result.setText(String.valueOf(output));
+            operator = "";
+            start = true;
+        }
+        
+    }
+}
